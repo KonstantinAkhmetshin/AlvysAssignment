f326b6c919281c44d1e86d8784b0890eaa510c69
KonstantinAkhmetshin <konstantin.akhmetshin@gmail.com>
2023-08-18 13:15:00 -0700

    unit tests are implemented

--- Assignment.Main/Functions.cs
+++ Assignment.Main/Functions.cs
@@ -1,93 +1,102 @@
 ﻿
 using System.Text;
-class Functions
+
+namespace Assignment.Main
 {
-
-    public static void Main(string[] args){
-        Console.WriteLine(Map(new List<int>(){1,2,3}, x => x +2));
-    }
-
-    private static List<B> Map<A, B>(List<A> list, Func<A, B> f)
+    public class Functions
     {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Alvys");
+        }
 
-        if (list?.Any() != true)
+        public static List<B> Map<A, B>(List<A> list, Func<A, B> f)
         {
-            return new List<B>();
+
+            if (list?.Any() != true)
+            {
+                return new List<B>();
+            }
+            if (f == null)
+            {
+                throw new ArgumentNullException("Input function cannot be null.");
+            }
+            var result = new List<B>(list.Count());
+            foreach (var element in list)
+            {
+                if (element == null) continue;
+                result.Add(f(element));
+            }
+            return result;
         }
-        if (f == null)
+
+        public static B Fold<A, B>(List<A> list, B initial, Func<B, A, B> folder)
         {
-            throw new ArgumentNullException("Input function cannot be null.");
+            if (list?.Any() != true) return initial;
+            if (folder == null)
+            {
+                throw new ArgumentNullException("Input function cannot be null.");
+            }
+            if (initial == null)
+            {
+                throw new ArgumentNullException("Input initial cannot be null.");
+            }
+            B result = initial;
+            foreach (var element in list)
+            {
+                if (element == null) continue;
+                result = folder(result, element);
+            }
+            return result;
         }
-        var result = new List<B>(list.Count());
-        foreach (var element in list)
+
+        // Strings are immutable. In the case of a very large input array memory issues could be faced. StringBuilder must be used for string arrays.
+        public static string Fold<A>(List<A> list, string initial, Func<string, A, string> folder)
         {
-            if (element == null) continue;
-            result.Add(f(element));
+            if (list?.Any() != true) return initial;
+            if (folder == null)
+            {
+                throw new ArgumentNullException("Input function cannot be null.");
+            }
+            if (initial == null)
+            {
+                throw new ArgumentNullException("Input initial cannot be null.");
+            }
+            StringBuilder result = new StringBuilder(initial);
+            foreach (var element in list)
+            {
+                if (element == null) continue;
+                result.Append(folder(initial, element));
+            }
+            return result.ToString();
         }
-        return result;
+
+        public static List<B> Map2<A, B>(List<A> list, Func<A, B> f)
+        {
+            if (list?.Any() != true)
+            {
+                return new List<B>();
+            }
+            if (f == null)
+            {
+                throw new ArgumentNullException("Input function cannot be null.");
+            }
+
+            return Fold(list, new List<B>(), (state, element) =>
+            {
+                state.Add(f(element));
+                return state;
+            });
+
+            // OR that way 
+            // var result = new List<B>(list.Count());
+            // var emptyList = new List<A>();
+            // foreach (var element in list)
+            // {
+            //     result.Add(Fold(emptyList, f(element), (state, element) => state));
+            // }
+            // return result;
+        }
+
     }
-
-
-
-    private static B Fold<A, B>(List<A> list, B initial, Func<B, A, B> folder)
-    {
-        if (list?.Any() != true) return initial;
-        if (folder == null)
-        {
-            throw new ArgumentNullException("Input function cannot be null.");
-        }
-        if (initial == null)
-        {
-            throw new ArgumentNullException("Input initial cannot be null.");
-        }
-        B result = initial;
-        foreach (var element in list)
-        {
-            if (element == null) continue;
-            result = folder(result, element);
-        }
-        return result;
-    }
-
-    // Strings are immutable. StringBuilder must be used for string arrays.
-    private static string Fold<A>(List<A> list, string initial, Func<string, A, string> folder)
-    {
-        if (list?.Any() != true) return initial;
-        if (folder == null)
-        {
-            throw new ArgumentNullException("Input function cannot be null.");
-        }
-        if (initial == null)
-        {
-            throw new ArgumentNullException("Input initial cannot be null.");
-        }
-        StringBuilder result = new StringBuilder(initial);
-        foreach (var element in list)
-        {
-            if (element == null) continue;
-            result.Append(folder(initial, element));
-        }
-        return result.ToString();
-    }
-
-    private static List<B> Map2<A, B>(List<A> list, Func<A, B> f)
-    {
-        if (list?.Any() != true)
-        {
-            return new List<B>();
-        }
-        if (f == null)
-        {
-            throw new ArgumentNullException("Input function cannot be null.");
-        }
-        var result = new List<B>(list.Count());
-        var emptyList = new List<A>();
-        foreach (var element in list)
-        {
-            var trick = f(element);
-            result.Add(Fold(emptyList, trick, (sum, x) => sum));
-        }
-        return result;
-    }
-
 }
---
+++ Assignment.Test/Functions.Test.cs
@@ -0,0 +1,151 @@
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+
+
+namespace Assignment.Test;
+
+public class Functions_Test
+{
+
+    [Test]
+    public void Map_AddOne()
+    {
+        var numbers = new List<int> { 1, 2, 3 };
+        var result = Assignment.Main.Functions.Map(numbers, x => x + 1);
+        var expected = new List<int> { 2, 3, 4 };
+        Assert.AreEqual(expected, result);
+    }
+
+    [Test]
+    public void Map_ToString()
+    {
+        var numbers = new List<int> { 1, 2, 3 };
+        var result = Assignment.Main.Functions.Map(numbers, x => x.ToString());
+        var expected = new List<string> { "1", "2", "3" };
+        Assert.AreEqual(expected, result);
+    }
+
+
+    [Test]
+    public void Map_NullValue_Ommited()
+    {
+        var numbers = new List<string> { "1", null, "3" };
+        var result = Assignment.Main.Functions.Map(numbers, x => x.ToString());
+        var expected = new List<string> { "1", "3" };
+        Assert.AreEqual(expected, result);
+    }
+
+    [Test]
+    public void Map_EmptyList()
+    {
+        var emptyList = new List<int>();
+        var result = Assignment.Main.Functions.Map(emptyList, x => x + 1);
+        Assert.IsEmpty(result);
+    }
+
+    [Test]
+    public void Map_NullList_Returns_EmptyList()
+    {
+        List<int> emptyList = null;
+        var result = Assignment.Main.Functions.Map(emptyList, x => x + 1);
+        Assert.IsEmpty(result);
+    }
+
+    [Test]
+    public void Fold_Sum()
+    {
+        List<int> numbers = new List<int> { 1, 2, 3 };
+        int result = Assignment.Main.Functions.Fold(numbers, 0, (sum, x) => sum + x);
+        Assert.AreEqual(6, result);
+    }
+
+    [Test]
+    public void Fold_ToString()
+    {
+        var numbers = new List<int> { 1, 2, 3 };
+        string result = Assignment.Main.Functions.Fold(numbers, "", (str, x) => str + x.ToString());
+        Assert.AreEqual("123", result);
+    }
+
+
+    [Test]
+    public void Fold_NullValues_Ommited()
+    {
+        var numbers = new List<string> { "1", null, "3" };
+        string result = Assignment.Main.Functions.Fold(numbers, "", (str, x) => str + x.ToString());
+        Assert.AreEqual("13", result);
+    }
+
+    [Test]
+    public void Fold_EmptyList_ReturnsInitial()
+    {
+        var emptyList = new List<int>();
+        int init = 10;
+        int result = Assignment.Main.Functions.Fold(emptyList, init, (sum, x) => sum + x);
+        Assert.AreEqual(init, result);
+    }
+
+    [Test]
+    public void Fold_NullList_ReturnsInitial()
+    {
+        List<int> nullList = null;
+        int init = 10;
+        int result = Assignment.Main.Functions.Fold(nullList, init, (sum, x) => sum + x);
+        Assert.AreEqual(init, result);    
+    }
+
+    [Test]
+    public void Fold_NullInitial_ThrowsArgumentNullException()
+    {
+        var numbers = new List<int> { 1, 2, 3 };
+        string init = null;
+        Assert.Throws<ArgumentNullException>(() => Assignment.Main.Functions.Fold(numbers, init, (sum, x) => sum + x));
+    }
+
+    [Test]
+    public void Map2_AddOne()
+    {
+        var numbers = new List<int> { 1, 2, 3 };
+        var result = Assignment.Main.Functions.Map2(numbers, x => x + 1);
+        var expected = new List<int> { 2, 3, 4 };
+        Assert.AreEqual(expected, result);
+    }
+
+    [Test]
+    public void Map2_ToString()
+    {
+        var numbers = new List<int> { 1, 2, 3 };
+        var result = Assignment.Main.Functions.Map2(numbers, x => x.ToString());
+        var expected = new List<string> { "1", "2", "3" };
+        Assert.AreEqual(expected, result);
+    }
+
+
+    [Test]
+    public void Map2_NullValue_Ommited()
+    {
+        var numbers = new List<string> { "1", null, "3" };
+        var result = Assignment.Main.Functions.Map2(numbers, x => x.ToString());
+        var expected = new List<string> { "1", "3" };
+        Assert.AreEqual(expected, result);
+    }
+
+    [Test]
+    public void Map2_EmptyList()
+    {
+        var emptyList = new List<int>();
+        var result = Assignment.Main.Functions.Map2(emptyList, x => x + 1);
+        Assert.IsEmpty(result);
+    }
+
+    [Test]
+    public void Map2_NullList_Returns_EmptyList()
+    {
+        List<int> emptyList = null;
+        var result = Assignment.Main.Functions.Map2(emptyList, x => x + 1);
+        Assert.IsEmpty(result);
+    }
+
+
+}
